25d1d24e52b0f108e3e854cf3d57fe32610d763e
ugarth-p <aniskebbab01@gmail.com>
2026-07-13 17:46:37 +0100

    add OOP hierarchy

---
+++ src/application/Attackable.java
@@ -0,0 +1,7 @@
+package application;
+
+public interface Attackable {
+	void attack(Character target);
+
+	void useSpecialAbility(Character target);
+}
---
+++ src/application/Character.java
@@ -0,0 +1,55 @@
+package application;
+
+abstract class Character implements Attackable {
+
+	protected String name;
+	protected int health;
+	protected int damage;
+	protected int level;
+	protected int experience;
+	protected int maxHealth = 100;
+	protected int x;
+	protected int y;
+
+	public Character(String name, int health, int damage, int x, int y) {
+		this.name = name;
+		this.health = health;
+		this.damage = damage;
+		this.maxHealth = maxHealth;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void receiveDamage(int damage) {
+		health -= damage;
+		System.out.println(name + " received " + damage + " damage. HP remaining: " + health);
+	}
+
+	public boolean isAlive() {
+		return health > 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
